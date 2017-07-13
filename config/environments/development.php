52540e10eb0e6157d84269eca07d0b884d597269
Dave Kiss <dave@davekiss.com>
2017-07-13 17:11:19 -0400

    Initial commit

---
+++ config/environments/development.php
@@ -0,0 +1,10 @@
+<?php
+/** Development */
+define('SAVEQUERIES', true);
+define('WP_DEBUG', true);
+define('SCRIPT_DEBUG', true);
+
+/* Inserted by Local by Flywheel. See: http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy */
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+  $_SERVER['HTTPS'] = 'on';
+}
