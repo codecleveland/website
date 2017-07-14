cdc549ff6db93915774dc4ade20bdd1e8e92903e
Dave Kiss <dave@davekiss.com>
2017-07-13 20:00:53 -0400

    Add tech-community theme

---
+++ web/app/themes/tech-community/app/setup.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Container\Container as ContainerContract;
+use Roots\Sage\Assets\JsonManifest;
+use Roots\Sage\Config;
+use Roots\Sage\Template\Blade;
+use Roots\Sage\Template\BladeProvider;
+
+/**
+ * Theme assets
+ */
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+}, 100);
+
+/**
+ * Theme setup
+ */
+add_action('after_setup_theme', function () {
+    /**
+     * Enable features from Soil when plugin is activated
+     * @link https://roots.io/plugins/soil/
+     */
+    add_theme_support('soil-clean-up');
+    add_theme_support('soil-jquery-cdn');
+    add_theme_support('soil-nav-walker');
+    add_theme_support('soil-nice-search');
+    add_theme_support('soil-relative-urls');
+
+    /**
+     * Enable plugins to manage the document title
+     * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
+     */
+    add_theme_support('title-tag');
+
+    /**
+     * Register navigation menus
+     * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+     */
+    register_nav_menus([
+        'primary_navigation' => __('Primary Navigation', 'sage')
+    ]);
+
+    /**
+     * Enable post thumbnails
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
+    add_theme_support('post-thumbnails');
+
+    /**
+     * Enable HTML5 markup support
+     * @link https://developer.wordpress.org/reference/functions/add_theme_support/#html5
+     */
+    add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+
+    /**
+     * Enable selective refresh for widgets in customizer
+     * @link https://developer.wordpress.org/themes/advanced-topics/customizer-api/#theme-support-in-sidebars
+     */
+    add_theme_support('customize-selective-refresh-widgets');
+
+    /**
+     * Use main stylesheet for visual editor
+     * @see resources/assets/styles/layouts/_tinymce.scss
+     */
+    add_editor_style(asset_path('styles/main.css'));
+}, 20);
+
+/**
+ * Register sidebars
+ */
+add_action('widgets_init', function () {
+    $config = [
+        'before_widget' => '<section class="widget %1$s %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>'
+    ];
+    register_sidebar([
+        'name'          => __('Primary', 'sage'),
+        'id'            => 'sidebar-primary'
+    ] + $config);
+    register_sidebar([
+        'name'          => __('Footer', 'sage'),
+        'id'            => 'sidebar-footer'
+    ] + $config);
+});
+
+/**
+ * Updates the `$post` variable on each iteration of the loop.
+ * Note: updated value is only available for subsequently loaded views, such as partials
+ */
+add_action('the_post', function ($post) {
+    sage('blade')->share('post', $post);
+});
+
+/**
+ * Setup Sage options
+ */
+add_action('after_setup_theme', function () {
+    /**
+     * Sage config
+     */
+    $paths = [
+        'dir.stylesheet' => get_stylesheet_directory(),
+        'dir.template'   => get_template_directory(),
+        'dir.upload'     => wp_upload_dir()['basedir'],
+        'uri.stylesheet' => get_stylesheet_directory_uri(),
+        'uri.template'   => get_template_directory_uri(),
+    ];
+    $viewPaths = collect(preg_replace('%[\/]?(resources/views)?[\/.]*?$%', '', [STYLESHEETPATH, TEMPLATEPATH]))
+        ->flatMap(function ($path) {
+            return ["{$path}/resources/views", $path];
+        })->unique()->toArray();
+
+    config([
+        'assets.manifest' => "{$paths['dir.stylesheet']}/../dist/assets.json",
+        'assets.uri'      => "{$paths['uri.stylesheet']}/dist",
+        'view.compiled'   => "{$paths['dir.upload']}/cache/compiled",
+        'view.namespaces' => ['App' => WP_CONTENT_DIR],
+        'view.paths'      => $viewPaths,
+    ] + $paths);
+
+    /**
+     * Add JsonManifest to Sage container
+     */
+    sage()->singleton('sage.assets', function () {
+        return new JsonManifest(config('assets.manifest'), config('assets.uri'));
+    });
+
+    /**
+     * Add Blade to Sage container
+     */
+    sage()->singleton('sage.blade', function (ContainerContract $app) {
+        $cachePath = config('view.compiled');
+        if (!file_exists($cachePath)) {
+            wp_mkdir_p($cachePath);
+        }
+        (new BladeProvider($app))->register();
+        return new Blade($app['view'], $app);
+    });
+
+    /**
+     * Create @asset() Blade directive
+     */
+    sage('blade')->compiler()->directive('asset', function ($asset) {
+        return "<?= App\\asset_path({$asset}); ?>";
+    });
+});
+
+/**
+ * Init config
+ */
+sage()->bindIf('config', Config::class, true);
+
+
+add_action('init', function() {
+
+    /**
+     * meetup_group CPT
+     */
+    $labels = array(
+      'name'                => _x( 'Meetup Groups', 'Post Type General Name', 'tech-community' ),
+      'singular_name'       => _x( 'Meetup Group', 'Post Type Singular Name', 'tech-community' ),
+      'menu_name'           => __( 'Meetup Groups', 'tech-community' ),
+      'parent_item_colon'   => __( 'Parent Meetup Group:', 'tech-community' ),
+      'all_items'           => __( 'Meetup Groups', 'tech-community' ),
+      'view_item'           => __( 'View Meetup Group', 'tech-community' ),
+      'add_new_item'        => __( 'Add New Meetup Group', 'tech-community' ),
+      'add_new'             => __( 'Add New', 'tech-community' ),
+      'edit_item'           => __( 'Edit Meetup Group', 'tech-community' ),
+      'update_item'         => __( 'Update Meetup Group', 'tech-community' ),
+      'search_items'        => __( 'Search Meetup Groups', 'tech-community' ),
+      'not_found'           => __( 'Not found', 'tech-community' ),
+      'not_found_in_trash'  => __( 'Not found in Trash', 'tech-community' ),
+    );
+
+    $args = array(
+      'label'               => $labels['name'],
+      'description'         => __( 'Tech-specific meetup groups that exist in Northeast Ohio', 'tech-community' ),
+      'labels'              => $labels,
+      'supports'            => array( 'title', 'author', 'thumbnail', 'revisions', 'page-attributes', ),
+      'taxonomies'          => array( 'category', 'post_tag' ),
+      'rewrite'             => array( 'slug' => 'meetup-groups', 'with_front' => FALSE ),
+      'hierarchical'        => true,
+      'public'              => true,
+      'show_ui'             => true,
+      'show_in_menu'        => true,
+      'show_in_nav_menus'   => true,
+      'show_in_admin_bar'   => true,
+      'show_in_rest'        => true,
+      'rest_base'           => 'meetup-groups',
+      'can_export'          => true,
+      'has_archive'         => true,
+      'exclude_from_search' => false,
+      'publicly_queryable'  => true,
+      'map_meta_cap'        => true,
+      'capability_type'     => 'meetup_group',
+    );
+
+    register_post_type( 'meetup_group', $args );
+
+
+    /**
+     * event CPT
+     */
+    $labels = array(
+      'name'                => _x( 'Events', 'Post Type General Name', 'tech-community' ),
+      'singular_name'       => _x( 'Event', 'Post Type Singular Name', 'tech-community' ),
+      'menu_name'           => __( 'Events', 'tech-community' ),
+      'parent_item_colon'   => __( 'Parent Event:', 'tech-community' ),
+      'all_items'           => __( 'Events', 'tech-community' ),
+      'view_item'           => __( 'View Event', 'tech-community' ),
+      'add_new_item'        => __( 'Add New Event', 'tech-community' ),
+      'add_new'             => __( 'Add New', 'tech-community' ),
+      'edit_item'           => __( 'Edit Event', 'tech-community' ),
+      'update_item'         => __( 'Update Event', 'tech-community' ),
+      'search_items'        => __( 'Search Events', 'tech-community' ),
+      'not_found'           => __( 'Not found', 'tech-community' ),
+      'not_found_in_trash'  => __( 'Not found in Trash', 'tech-community' ),
+    );
+
+    $args = array(
+      'label'               => $labels['name'],
+      'description'         => __( 'Tech-specific events that are happening in Northeast Ohio', 'tech-community' ),
+      'labels'              => $labels,
+      'supports'            => array( 'title', 'author', 'thumbnail', 'revisions', 'page-attributes', ),
+      'taxonomies'          => array( 'category', 'post_tag' ),
+      'rewrite'             => array( 'slug' => 'events', 'with_front' => FALSE ),
+      'hierarchical'        => true,
+      'public'              => true,
+      'show_ui'             => true,
+      'show_in_menu'        => true,
+      'show_in_nav_menus'   => true,
+      'show_in_admin_bar'   => true,
+      'show_in_rest'        => true,
+      'rest_base'           => 'events',
+      'can_export'          => true,
+      'has_archive'         => true,
+      'exclude_from_search' => false,
+      'publicly_queryable'  => true,
+      'map_meta_cap'        => true,
+      'capability_type'     => 'event',
+    );
+
+    register_post_type( 'event', $args );
+
+
+    /**
+     * sponsor CPT
+     */
+    $labels = array(
+      'name'                => _x( 'Sponsors', 'Post Type General Name', 'tech-community' ),
+      'singular_name'       => _x( 'Sponsor', 'Post Type Singular Name', 'tech-community' ),
+      'menu_name'           => __( 'Sponsors', 'tech-community' ),
+      'parent_item_colon'   => __( 'Parent Sponsor:', 'tech-community' ),
+      'all_items'           => __( 'Sponsors', 'tech-community' ),
+      'view_item'           => __( 'View Sponsor', 'tech-community' ),
+      'add_new_item'        => __( 'Add New Sponsor', 'tech-community' ),
+      'add_new'             => __( 'Add New', 'tech-community' ),
+      'edit_item'           => __( 'Edit Sponsor', 'tech-community' ),
+      'update_item'         => __( 'Update Sponsor', 'tech-community' ),
+      'search_items'        => __( 'Search Sponsors', 'tech-community' ),
+      'not_found'           => __( 'Not found', 'tech-community' ),
+      'not_found_in_trash'  => __( 'Not found in Trash', 'tech-community' ),
+    );
+
+    $args = array(
+      'label'               => $labels['name'],
+      'description'         => __( 'Companies and individuals that sponsor Code Cleveland.', 'tech-community' ),
+      'labels'              => $labels,
+      'supports'            => array( 'title', 'author', 'thumbnail', 'revisions', 'page-attributes', ),
+      'taxonomies'          => array( 'category', 'post_tag' ),
+      'rewrite'             => array( 'slug' => 'sponsors', 'with_front' => FALSE ),
+      'hierarchical'        => true,
+      'public'              => true,
+      'show_ui'             => true,
+      'show_in_menu'        => true,
+      'show_in_nav_menus'   => true,
+      'show_in_admin_bar'   => true,
+      'show_in_rest'        => true,
+      'rest_base'           => 'sponsors',
+      'can_export'          => true,
+      'has_archive'         => true,
+      'exclude_from_search' => false,
+      'publicly_queryable'  => true,
+      'map_meta_cap'        => true,
+      'capability_type'     => 'sponsor',
+    );
+
+    register_post_type( 'sponsor', $args );
+
+});
---
+++ web/app/themes/tech-community/resources/views/front-page.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+  <header class="sans-serif">
+    <div class="cover bg-left bg-center-l" style="background-image: url(https://api.mapbox.com/styles/v1/davekiss/cj4zw77tu13jn2sqo7xuhsl35/static/-81.695124,41.415285,9.00,0.00,0.00/1000x500@2x?access_token=pk.eyJ1IjoiZGF2ZWtpc3MiLCJhIjoiY2owNjAxZW5iMGt4MDMzcDMxZTRhYWdreiJ9.Ta-pzKAzPcFZ0aP0tjrP5Q)">
+      <div class="bg-black-80 pb5 pb6-m pb7-l">
+        <nav class="dt w-100 mw8 center">
+          <div class="dtc w2 v-mid pa3">
+            <a href="{{ home_url('/') }}" class="dib w2 h2 pa1 ba b--white-90 grow-large border-box">
+              <svg class="link white-90 hover-white" data-icon="skull" viewBox="0 0 32 32" style="fill:currentcolor"><title>skull icon</title><path d="M16 0 C6 0 2 4 2 14 L2 22 L6 24 L6 30 L26 30 L26 24 L30 22 L30 14 C30 4 26 0 16 0 M9 12 A4.5 4.5 0 0 1 9 21 A4.5 4.5 0 0 1 9 12 M23 12 A4.5 4.5 0 0 1 23 21 A4.5 4.5 0 0 1 23 12"></path></svg>
+            </a>
+          </div>
+          <div class="dtc v-mid tr pa3">
+            <a class="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" href="/meetup-groups">Meetup Groups</a>
+            <a class="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" href="/sponsors">Sponsors</a>
+            <a class="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" href="/events">Events</a>
+            <a class="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" href="/contact">Contact</a>
+            <a class="f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba" href="/join">Sign Up</a>
+          </div>
+        </nav>
+        <div class="tc-l mt4 mt5-m mt6-l ph3">
+          <h1 class="f2 f1-l white-90 mb0 lh-title b">Code Cleveland</h1>
+          <h2 class="fw1 f3 white-80 mt3 mb4">Supporting the Cleveland tech community.</h2>
+          <a class="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="/join">Create a Profile</a>
+          <span class="dib v-mid ph3 white-70 mb3">or</span>
+          <a class="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </header>
+@endsection
