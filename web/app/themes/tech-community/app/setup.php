3b601ee8513f815700823714da610d97c195e61b
Dave Kiss <dave@davekiss.com>
2017-07-13 21:05:28 -0400

    Add Job CPT

--- web/app/themes/tech-community/app/setup.php
+++ web/app/themes/tech-community/app/setup.php
@@ -293,4 +293,48 @@ add_action('init', function() {
 
     register_post_type( 'sponsor', $args );
 
+    /**
+     * job CPT
+     */
+    $labels = array(
+      'name'                => _x( 'Jobs', 'Post Type General Name', 'tech-community' ),
+      'singular_name'       => _x( 'Job', 'Post Type Singular Name', 'tech-community' ),
+      'menu_name'           => __( 'Jobs', 'tech-community' ),
+      'parent_item_colon'   => __( 'Parent Job:', 'tech-community' ),
+      'all_items'           => __( 'Jobs', 'tech-community' ),
+      'view_item'           => __( 'View Job', 'tech-community' ),
+      'add_new_item'        => __( 'Add New Job', 'tech-community' ),
+      'add_new'             => __( 'Add New', 'tech-community' ),
+      'edit_item'           => __( 'Edit Job', 'tech-community' ),
+      'update_item'         => __( 'Update Job', 'tech-community' ),
+      'search_items'        => __( 'Search Jobs', 'tech-community' ),
+      'not_found'           => __( 'Not found', 'tech-community' ),
+      'not_found_in_trash'  => __( 'Not found in Trash', 'tech-community' ),
+    );
+
+    $args = array(
+      'label'               => $labels['name'],
+      'description'         => __( 'Companies and individuals that are hiring in Cleveland.', 'tech-community' ),
+      'labels'              => $labels,
+      'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', 'page-attributes', ),
+      'taxonomies'          => array( 'category', 'post_tag' ),
+      'rewrite'             => array( 'slug' => 'jobs', 'with_front' => FALSE ),
+      'hierarchical'        => true,
+      'public'              => true,
+      'show_ui'             => true,
+      'show_in_menu'        => true,
+      'show_in_nav_menus'   => true,
+      'show_in_admin_bar'   => true,
+      'show_in_rest'        => true,
+      'rest_base'           => 'jobs',
+      'can_export'          => true,
+      'has_archive'         => true,
+      'exclude_from_search' => false,
+      'publicly_queryable'  => true,
+      'map_meta_cap'        => true,
+      'capability_type'     => 'post',
+    );
+
+    register_post_type( 'job', $args );
+
 });
