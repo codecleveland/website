a1bdc1966fb79efa3422e22cbe1a6a3420712680
Dave Kiss <dave@davekiss.com>
2017-07-21 11:10:14 -0500

    Change CTA text

--- web/app/themes/tech-community/resources/views/front-page.blade.php
+++ web/app/themes/tech-community/resources/views/front-page.blade.php
@@ -21,7 +21,7 @@
         <div class="tc-l mt4 mt5-m mt6-l ph3">
           <h1 class="f2 f1-l white-90 mb0 lh-title b">Code Cleveland</h1>
           <h2 class="fw1 f3 white-80 mt3 mb4">Supporting the Cleveland tech community.</h2>
-          <a class="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="/join">Create a Profile</a>
+          <a class="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="/join">Join the Community</a>
           <span class="dib v-mid ph3 white-70 mb3">or</span>
           <a class="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="">Learn More</a>
         </div>
