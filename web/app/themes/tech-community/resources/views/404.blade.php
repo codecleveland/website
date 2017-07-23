18446807f748f8e4c6eb5a63ab62ff3eed9bf2a7
Dave Kiss <dave@davekiss.com>
2017-07-23 10:22:17 -0400

    Add 404 template

--- web/app/themes/tech-community/resources/views/404.blade.php
+++ web/app/themes/tech-community/resources/views/404.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
-
-  {!! get_the_posts_navigation() !!}
+	<section class="pb5 bg-white baskerville">
+		<header class="tc ph5 lh-copy">
+			<h1 class="f1 f-headline-l code mb3 fw9 dib tracked-tight light-purple">404</h1>
+			<h2 class="tc f1-l fw1">Sorry, we can't find the page you are looking for.</h2>
+		</header>
+		<p class="fw1 i tc mt4 mt5-l f4 f3-l">Are you looking for one of these?</p>
+		<ul class="list tc pl0 w-100 mt5">
+			<li class="dib"><a class="f5 f4-ns link black db pv2 ph3 hover-light-purple" href="/">Home</a></li>
+			<li class="dib"><a class="f5 f4-ns link black db pv2 ph3 hover-light-purple" href="/events">Events</a></li>
+			<li class="dib"><a class="f5 f4-ns link black db pv2 ph3 hover-light-purple" href="/contact">Contact</a></li>
+			<li class="dib"><a class="f5 f4-ns link black db pv2 ph3 hover-light-purple" href="/join">Join the Community</a></li>
+		</ul>
+	</section>
 @endsection
