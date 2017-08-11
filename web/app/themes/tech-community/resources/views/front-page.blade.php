161825c5c3ad224b4db9ea60f2e8f46bf6bb002b
Dave Kiss <dave@davekiss.com>
2017-08-11 14:29:31 -0400

    Build out the home page with better copy

--- web/app/themes/tech-community/resources/views/front-page.blade.php
+++ web/app/themes/tech-community/resources/views/front-page.blade.php
@@ -65,7 +65,7 @@
     </div>
   </header>
   <div class="dt mw9 center pt0 pb5 pv5-m pv5-ns">
-    <div class="db dtc-ns v-mid ph2 pr2-ns pl3-ns measure">
+    <div class="db dtc-ns v-mid ph2 pr2-ns pl3-ns measure fl w-100 w-50-ns pa2">
       <h3 class="f3 fw7 ttu tracked lh-title mt0 mb3 avenir">For the dev community, <br />by the dev community</h3>
       <p class="f4 lh-copy">
         At our core, Code Cleveland is about building the community. We strive to bring people together from different backgrounds, disciplines, and skill levels to share their ideas, learn about new topics, and create lasting relationships.
@@ -74,8 +74,69 @@
         We started this initiative to connect bright minds and provide an outlet for those who leverage technology to make Cleveland a better place.
       </p>
     </div>
-    <div class="db dtc-ns v-mid-ns">
-      <img src="@asset('images/community.jpg')" alt="A bright blue sky" class="w-100 mw6" />
+    <div class="db dtc-ns v-mid-ns fl w-100 w-50-ns pa2">
+      <img src="@asset('images/community.jpg')" alt="Community" class="w-100 mw6" />
     </div>
+  </div>
+
+  <div class="dt mw9 center pt0 pb5 pv5-m pv5-ns">
+    <div class="db dtc-ns v-mid-ns fl w-100 w-50-ns pa2">
+      <img src="@asset('images/two-men-working.jpg')" alt="Get things done" class="w-100 mw6" />
+    </div>
+    <div class="db dtc-ns v-top ph2 pr2-ns pl3-ns measure fl w-100 w-50-ns pa2">
+      <h3 class="f3 fw7 ttu tracked lh-title mt0 mb3 avenir">Get Things Done</h3>
+      <p class="f4 lh-copy">
+        Looking for work? Our <a href="/jobs" title="Cleveland Tech Job Board" class="link dim blue">local job board</a> lists some of the open positions available in the Cleveland tech community.
+      </p>
+      <p class="f4 lh-copy">
+        Whether you need an office for a day or a month, <a href="/workspaces" title="Cleveland Tech Coworking Spaces" class="link dim blue">our partner workspaces around town</a> provide a friendly environment to focus on your productivity.
+      </p>
+    </div>
+  </div>
+
+  <div class="dt mw9 center pt0 pb5 pv5-m pv5-ns">
+    <div class="db dtc-ns v-mid ph2 pr2-ns pl3-ns measure fl w-100 w-50-ns pa2">
+      <h3 class="f3 fw7 ttu tracked lh-title mt0 mb3 avenir">Learn to Code</h3>
+      <p class="f4 lh-copy">
+        We were all beginners once. Take advantage of our <a href="/resources" title="Cleveland Code Resources" class="link dim blue">resources</a>, sign up for a <a href="/workshops" title="Cleveland Code Workshops" class="link dim blue">workshop</a>, or <a href="/bootcamps" title="Cleveland Code Bootcamps" class="link dim blue">enroll in a partner bootcamp.</a>
+      </p>
+    </div>
+    <div class="db dtc-ns v-mid-ns fl w-100 w-50-ns pa2">
+      <img src="@asset('images/coding.jpg')" alt="Learn to Code" class="w-100 mw6" />
+    </div>
+  </div>
+
+  <div class="vh-25 vh-50-m vh-75-ns bg-light-pink dt w-100">
+    <div 
+      style="background:url(@asset('images/rock-and-roll-hall-of-fame.jpg')) no-repeat center right;background-size: cover;" 
+      class="dtc v-top cover pt4 pb3 ph4-m ph5-l">
+      <h1 class="f2 f-subheadline-l measure lh-title fw7 avenir ttu tracked">(Re)discover Cleveland</h1>
+      <h2 class="f3 fw6 pv4 ph5 black measure-wide lh-copy bg-white-40">A lot has changed around here over the past few years. Check out our <a href="/happy-hour" title="Cleveland Code Happy Hour" class="link dim blue">monthly Happy Hour</a>, <a href="/events" title="Cleveland Code Events" class="link dim blue">free events</a> and <a href="/meetup-groups" title="Cleveland Code Meetups" class="link dim blue">meetup groups</a> and come say hi and see what's new.</h2>
+    </div>
+  </div>
+
+  <div class="dt mw9 center pt0 pb5 pv5-m pv5-ns">
+    <div class="db dtc-ns v-mid ph2 pr2-ns pl3-ns measure fl w-100 w-50-ns pa2">
+      <h3 class="f3 fw7 ttu tracked lh-title mt0 mb3 avenir">Want to Get Involved?</h3>
+      <p class="f4 lh-copy">
+        We're looking for help with volunteers and sponsors to make Cleveland a better place to work in tech. <a href="/contact" title="Contact Cleveland Code" class="link dim blue">Contact us</a> if you'd like to get involved.
+      </p>
+    </div>
+    <div class="db dtc-ns v-mid-ns fl w-100 w-50-ns pa2">
+      <img src="@asset('images/desk-2.jpg')" alt="Want to Get Involved?" class="w-100 mw6" />
+    </div>
+  </div>
+
+  <div class="pa4-l">
+    <form class="bg-light-red mw7 center pa4 br2-ns ba b--black-10">
+      <fieldset class="cf bn ma0 pa0">
+        <legend class="pa0 f5 f4-ns mb3 black-80">Sign up for our newsletter</legend>
+        <div class="cf">
+          <label class="clip" for="email-address">Email Address</label>
+          <input class="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="Your Email Address" type="text" name="email-address" value="" id="email-address">
+          <input class="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="Subscribe">
+        </div>
+      </fieldset>
+    </form>
   </div>
 @endsection
